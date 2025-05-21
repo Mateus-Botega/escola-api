b89082003c35a61a04cec46258de6074875194f2
Mateus Botega <matibotega@outlook.com>
2025-05-21 20:18:50 -0300

    Commit inicial do projeto

---
+++ src/test/java/br/com/unisul/escolaapi/EscolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unisul.escolaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
